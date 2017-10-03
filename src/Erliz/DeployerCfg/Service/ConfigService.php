f59c3c420320a795dde70cf59e838f3faf9182e4
Esin Grigoriy <g.esin@corp.mail.ru>
2017-10-03 14:54:37 +0300

    Add twig filter `emit_yaml`

--- src/Erliz/DeployerCfg/Service/ConfigService.php
+++ src/Erliz/DeployerCfg/Service/ConfigService.php
@@ -178,6 +178,10 @@ class ConfigService
         $loader = new Twig_Loader_String();
         $twig = new Twig_Environment($loader, ['debug' => true, 'strict_variables' => true, 'autoescape' => false]);
 
+        $twig->addFilter(new Twig_SimpleFilter('emit_yaml', function ($value) {
+            return yaml_emit($value);
+        }));
+
         return $twig;
     }
 
